a824ee85821eb238599381cc1b13ea5a3a3a5d91
blestunix <95473302+blestunix@users.noreply.github.com>
2022-02-15 23:01:54 +0530

    Brute force

--- p19.swift
+++ p19.swift
@@ -11,3 +11,22 @@
  *  How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
  *
  */
+
+func solution() -> Int {
+  var n = 0, dow = 2;
+  var months = [31, 0, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+  for y  in 1901 ... 2000 {
+    months[1] = (y % 4 == 0 && y % 100 != 0 || y % 400 == 0) ? 29 : 28
+
+    for month in months {
+      dow += month % 7
+      if (dow % 7 == 0) {
+        n += 1
+      }
+    }
+  }
+  return n
+}
+
+print(solution())
